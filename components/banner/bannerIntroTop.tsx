882e765deb053c20c6ce790fe1a664881f43b30b
quanguway <quangnguyen.uway@gmail.com>
2022-12-17 00:01:06 +0700

    fix responsive

--- components/banner/bannerIntroTop.tsx
+++ components/banner/bannerIntroTop.tsx
@@ -8,13 +8,13 @@ export default function BannerIntroTop () {
     <div className="flex md:flex-row sm:flex-col sm:flex-col-reverse sm:items-center">
       <div className="flex items-center sm:flex sm:justify-center ">
         <div className="flex flex-col max-w-[630px] md:min-h-[386px] sm:justify-items-center">
-          <p className="font-bold text-primary text-6xl sm:mt-5 sm:text-3xl">
+          <p className="font-bold text-primary text-6xl md:text-4xl sm:mt-5 sm:text-3xl">
             Digital Marketing. Made Easy. 
           </p><br/>
-          <p className="text-2xl sm:text-lg">
+          <p className="text-2xl md:text-xl sm:text-lg">
             Automated digital marketing for small businesses & start-ups
           </p>
-          <p className="text-2xl sm:text-lg">
+          <p className="text-2xl md:text-xl sm:text-lg">
             Less work, better decisions, better results! 
           </p>
           <div className="mt-[36px] sm:flex sm:w-full">
@@ -30,7 +30,7 @@ export default function BannerIntroTop () {
           </div>
         </div>
       </div>
-      <div className="w-[600px] h-[417px] sm:w-[290px] sm:h-[190px] relative">
+      <div className="w-[600px] h-[417px] md:w-[390px] md:h-[290px] sm:w-[290px] sm:h-[190px] relative">
           <Image
             src='/img/homepage/header/illustration.png'
             alt='logo'
